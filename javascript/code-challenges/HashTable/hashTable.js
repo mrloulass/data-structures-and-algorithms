23c359ce7290fb5ef36712b1a50d408047ea4c09
Louis Lassegue <louislassegue@gmail.com>
2021-07-23 22:23:29 -0700

    hashtable implementation

---
+++ javascript/code-challenges/HashTable/hashTable.js
@@ -0,0 +1,86 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    if (!this.head) {
+      this.head = new Node(value);
+      return;
+    }
+    //traverse to the tail and add to the end
+    let current = this.head;
+    let previous = null;
+    while (current) {
+      previous = current;
+      current = current.next;
+    }
+    //current is null, how do we add to the list
+    previous.next = new Node(value);
+  }
+}
+
+
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.buckets = new Array(size);
+  }
+
+  hash(key) {
+    let arrayofCharacters = key.split('');
+    let sumofAsciiValues = arrayofCharacters.reduce((acc, char) => {
+      return acc + char.charCodeAt(0);
+    }, 0);
+    return (sumofAsciiValues * 599) % this.size;
+  }
+
+  add(key, value) {
+    let hash = this.hash(key);
+
+    if (!this.buckets[hash]) this.buckets[hash] = new LinkedList();
+
+    let entry = { [key]: value };
+
+    this.buckets[hash].insert({ [key]: value });
+    return entry;
+  }
+
+
+  // get a key , look up the value in our hash table and return
+  get(key) {
+    let hash = this.hash(key);
+    return this.buckets[hash];
+  }
+
+  // returns a boolean if the key exists
+  contains(key) {
+    let hash = this.hash(key);
+    return this.buckets[hash] ? true : false;
+  }
+
+}
+
+let table = new HashTable(1024);
+
+table.add('Louis', 'student');
+table.add('Mike', 'student');
+table.add('John', 'student');
+
+console.log(JSON.stringify(table));
+
+console.log(table.get('Louis'));
+console.log(table.get('Mike'));
+
+console.log(table.contains('John'));
+console.log(table.contains('Peter'));
---
+++ javascript/code-challenges/HashTable/hashTable.md
@@ -0,0 +1,48 @@
+# Hashtables
+Hash table is a data structure that implements an associative array abstract data type, a structure that can map keys to values. A hash table uses a hash function to compute an index, also called a hash code, into an array of buckets or slots, from which the desired value can be found.
+
+## Challenge
+New Implementation
+
+## Approach & Efficiency
+- searching for the element takes the same amount of time as searching for the first element of an array, which is a constant time or O(1)
+
+- hash table store the information by the hash value . so if there's no collision , it would take O(1).  
+
+## API
+Implement a Hashtable Class with methods
+- add()
+  - Arguments: key, value
+  - Returns: nothing
+  - This method should hash the key, and add the key and value pair to the table, handling collisions as needed.
+
+- get()
+  - Arguments: key
+  - Returns: Value associated with that key in the table
+
+- contains()
+  - Arguments: key
+  - Returns: Boolean, indicating if the key exists in the table already.
+
+- hash()
+  - Arguments: key
+  - Returns: Index in the collection for that key
+
+## Testing
+
+1. Adding a key/value to your hashtable results in the value being in the data structure 
+
+2. Retrieving based on a key returns the value stored
+
+3. Successfully returns null for a key that does not exist in the hashtable
+
+4. Successfully handle a collision within the hashtable
+
+5. Successfully retrieve a value from a bucket within the hashtable that has a collision
+
+6. Successfully hash a key to an in-range value
+
+## UML
+![Hash Table](hashTable.png)
+
+
