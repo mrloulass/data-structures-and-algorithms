29a16d1da18a229b4b0124514862450379d809f2
Louis Lassegue <louislassegue@gmail.com>
2021-07-31 17:34:38 -0700

    Quick Sort implementation blog

---
+++ javascript/code-challenges/Blog-code-challenge/Quick_Sort/Quick_Sort.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let arr = [8, 4, 23, 42, 16, 15];
+
+function quickSort(arr, left, right) {
+  if (left < right) {
+    let position = partition(arr, left, right);
+    quickSort(arr, left, position - 1);
+    quickSort(arr, position + 1, right);
+  }
+}
+
+function partition(arr, left, right) {
+  let pivot = arr[right];
+  let low = left - 1;
+  for (let i = left; i < right; i++) {
+    if (arr[i] <= pivot) {
+      low++;
+      swap(arr, i, low);
+    }
+  }
+  swap(arr, right, low + 1);
+
+  return low + 1;
+}
+
+function swap(arr, i, low) {
+  let temp;
+  temp = arr[i];
+  arr[i] = arr[low];
+  arr[low] = temp;
+}
+
+console.log(quickSort(arr));
